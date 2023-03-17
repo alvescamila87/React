f4f89be1709dedeaeb584266ab16ae21204d6717
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-03-17 20:31:23 -0300

    update react - componente, propriedade e estado

--- Discover/reactapp/src/pages/Home/index.jsx
+++ Discover/reactapp/src/pages/Home/index.jsx
@@ -1,12 +1,29 @@
+import React, { useState} from 'react';
+
 import './style.css'
 
+import { Card } from '../../components/Card'
+
 export function Home() {
-  
+
+  const [studentName, setStudentName] = useState()
+ 
   return (
     <div className='container'>
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite o nome..." />
+      <h1>Nome: {studentName}</h1>
+
+      <input 
+        type="text" 
+        placeholder="Digite o nome..." 
+        onChange={event => setStudentName(event.target.value)}
+      />
+
       <button type="button">Adicionar</button>
+
+      <Card name="Camila" time="10:55:25" />
+      <Card name="Isabel" time="11:00:10" />
+      <Card name="Karoline" time="11:05:10" />
+      <Card name="Paulo" time="12:35:00" />
     </div>
   )
 }
